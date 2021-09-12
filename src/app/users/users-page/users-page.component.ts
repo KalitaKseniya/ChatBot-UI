01f1c4a27f90039751ae90615e33ac9af70fb410
Kseniya Kalita <kseniya.kalita@hb.by>
2021-09-12 15:31:07 +0300

    users-page

--- src/app/users/users-page/users-page.component.ts
+++ src/app/users/users-page/users-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { User } from 'src/app/shared/interfaces';
 import { UsersService } from 'src/app/shared/services/users.service';
 
@@ -7,16 +8,34 @@ import { UsersService } from 'src/app/shared/services/users.service';
   templateUrl: './users-page.component.html',
   styleUrls: ['./users-page.component.scss']
 })
-export class UsersPageComponent implements OnInit {
+export class UsersPageComponent implements OnInit, OnDestroy {
 
   users: User[] = []
+  gSub: Subscription
+  dSub: Subscription
+
   constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
-    this.usersService.getUsers().subscribe((users: User[]) => {
+    this.gSub = this.usersService.getUsers().subscribe((users: User[]) => {
       this.users = users
     },
     (error) => console.log('Error when fetching users', error))
   }
 
+  deleteUser(id: string){
+    this.dSub = this.usersService.deleteUser(id).subscribe(() => {
+      this.users = this.users.filter(u => u.id !== id)
+    },
+    (error) => console.log('Error deleting user', error))
+  }
+
+  ngOnDestroy(){
+    if(this.dSub){
+      this.dSub.unsubscribe()
+    }
+     if(this.gSub){
+      this.gSub.unsubscribe()
+    }
+  }
 }
