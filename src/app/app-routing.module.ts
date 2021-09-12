7c5cc61c951ca42c4101eab015862aef51b356d6
Kseniya Kalita <kseniya.kalita@hb.by>
2021-09-12 11:49:01 +0300

    pages init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
+import { UsersPageComponent } from './users/users-page/users-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AdminLayoutComponent, children: [
+      //{path: '', redirectTo: 'admin/login', pathMatch: 'full'},
+      {path: 'login', component: LoginPageComponent},
+      {path: 'users', component: UsersPageComponent},
+      //{path: 'user/create', component: Use}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersPageComponent } from './users/users-page/users-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersPageComponent,
+    LoginPageComponent,
+    AdminLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
