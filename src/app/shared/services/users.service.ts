d5807175a864642522c7348e89ad43233caf5a77
Kseniya Kalita <kseniya.kalita@hb.by>
2021-09-12 12:44:16 +0300

    - add usersService - turn off strict checks - simple usersView

---
+++ src/app/shared/services/users.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User, UserForCreationDto, UserForUpdateDto } from '../interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<any>{
+    return this.http.get<User[]>(`${environment.serverUrl}/api/admin/users`)
+  }
+
+  createUser(userDto: UserForCreationDto): Observable<void>{
+    return this.http.post<void>(`${environment.serverUrl}/api/admin/users`, userDto)
+  }
+
+  getUserById(id: string): Observable<User>{
+    return this.http.get<User>(`${environment.serverUrl}/api/admin/users${id}`)
+  }
+
+  updateUser(id: string, userDto: UserForUpdateDto): Observable<User>{
+    return this.http.put<User>(`${environment.serverUrl}/api/admin/users/${id}`, userDto)
+  }
+
+  deleteUser(id: string): Observable<void>{
+    return this.http.delete<void>(`${environment.serverUrl}/api/admin/users/${id}`)
+  }
+
+
+}
